ad5781c5468aa83fc5548e8fae27891809177e73
Nischal <nyyscal541556@gmail.com>
2025-10-06 14:58:39 +0545

    Update Profile/Password + Nodemailer

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken"
+import { catchAsyncErrors } from "./catchAsyncError.js"
+import ErrorHandler from "./errorMiddleware.js"
+import database from "../db/db.js"
+
+export const isAuthenticated = catchAsyncErrors(async(req,res,next) =>{
+  const {token} = req.cookies;
+
+  if(!token) {
+    return next(new ErrorHandler("User not authenticated!",401))
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+
+  const user = await database.query(`SELECT * FROM users WHERE id = $1 LIMIT 1`,[decoded.id])
+  req.user = user.rows[0]
+  next()
+})
+
+export const authorizedRoles = (...roles) =>{
+  return (req,res,next) =>{
+    if(!roles.includes(req.user.role)){
+      return next(
+        new ErrorHandler(
+          `Role :${req.user.role} is not allowed to access this resource.`,
+          403
+        )
+      )
+    }
+    next()
+  }
+}
--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -1,11 +1,18 @@
 import express from "express"
-import { getUser, login, logout, register } from "../controllers/authController.js"
+import { forgotPassword, getUser, login, logout, register, resetPassword, updatePassword, updateProfile } from "../controllers/authController.js"
+import { isAuthenticated } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
 router.post("/register", register)
 router.post("/login", login)
-router.post("/logout", logout)
-router.post("/me", getUser)
+router.post("/password/forgot", forgotPassword)
+
+router.put("/password/reset/:token", resetPassword)
+router.put("/password/update",isAuthenticated, updatePassword)
+router.put("/profile/update",isAuthenticated, updateProfile)
+
+router.get("/me",isAuthenticated, getUser)
+router.get("/logout",isAuthenticated, logout)
 
 export default router
